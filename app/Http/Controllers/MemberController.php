ef8eff6078d38e2b446e99345b3d7d8d07becd5c
muhihsan1585-lab <muh.ihsan1585@gmail.com>
2026-09-18 19:02:08 +0700

    Add categories and members pages

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class MemberController extends Controller
 {
     public function index()
     {
-        return view('member.index');
+        $members = [
+            'Andi',
+            'Budi',
+            'Citra',
+            'Dewi',
+            'Eko'
+        ];
+        return view('members.index', compact('members'));
     }
-}
+};
